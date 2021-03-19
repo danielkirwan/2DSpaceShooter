25d130dc3aaca01bd8ddf683fe26115c94110b6b
Daniel Kirwan <danielkirwan07@gmail.com>
2021-03-19 16:29:43 +0000

    Player and Player script added

---
+++ 2D-SpaceShooter/Assets/Scripts/Player.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //set the players position when initiated 
+        transform.position = new Vector3(-8, 0, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Vector3.right is exquivalent of writing (1,0,0)
+        //This will move the object really fast
+        transform.Translate(Vector3.right * 5 * Time.deltaTime);
+    }
+}
