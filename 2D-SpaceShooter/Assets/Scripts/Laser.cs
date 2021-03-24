57200e1b15b38c86350e8853e2b6cef650b8617b
Daniel Kirwan <danielkirwan07@gmail.com>
2021-03-24 17:02:40 +0000

    The player can now shoot lasers

---
+++ 2D-SpaceShooter/Assets/Scripts/Laser.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Laser : MonoBehaviour
+{
+
+   [SerializeField] private float _speed = 3.5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(new Vector3(1,0,0)* _speed * Time.deltaTime,Space.World);
+    }
+}
--- 2D-SpaceShooter/Assets/Scripts/Player.cs
+++ 2D-SpaceShooter/Assets/Scripts/Player.cs
@@ -4,7 +4,11 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    [Header("Player speed")]
     [SerializeField] private float _speed;
+
+    [Header("GameObjects")]
+    public GameObject _laserPrefab;
 
     private float _horizontalMovement;
     private float _verticalMovement;
@@ -12,19 +16,43 @@ public class Player : MonoBehaviour
     void Start()
     {
         //set the players position when initiated 
-        transform.position = new Vector3(-8, 0, 0);
+        transform.position = new Vector3(-9, 0, 0);
     }
 
     // Update is called once per frame
     void Update()
     {
-        //Vector3.right is exquivalent of writing (1,0,0)
-        //This will move the object really fast
-       // transform.Translate(Vector3.right * _speed * Time.deltaTime);
+        PlayerMovement();
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            SpawnLaser();
+        }
+    }
+
+    void SpawnLaser()
+    {
+       // Instantiate(_laserPrefab, transform.position, Quaternion.Euler(0,0,-90));
+        Instantiate(_laserPrefab, transform.position, Quaternion.Euler(0,0,-90));
+    }
+
+    void PlayerMovement()
+    {
         _horizontalMovement = Input.GetAxis("Horizontal");
         _verticalMovement = Input.GetAxis("Vertical");
 
-        transform.Translate(new Vector3(_horizontalMovement,_verticalMovement,0) *_speed *Time.deltaTime);
+        transform.Translate(new Vector3(_horizontalMovement, _verticalMovement, 0) * _speed * Time.deltaTime);
+
+        if (transform.position.y >= 8)
+        {
+            transform.position = new Vector3(transform.position.x, -5f, transform.position.z);
+        }
+        else if (transform.position.y <= -5f)
+        {
+            transform.position = new Vector3(transform.position.x, 8f, transform.position.z);
+        }
+
+        transform.position = new Vector3(Mathf.Clamp(transform.position.x, -9.25f, 2), transform.position.y, transform.position.z);
 
     }
+
 }
