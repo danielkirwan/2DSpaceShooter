922c2906adb1d02322cd1b0a87d11e6c71fe1cc6
Daniel Kirwan <danielkirwan07@gmail.com>
2021-03-26 15:02:51 +0000

    enemy moves

---
+++ 2D-SpaceShooter/Assets/Scripts/Enemy.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] private float _speed = 3.5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(new Vector3(-1, 0, 0) * _speed * Time.deltaTime);
+        
+        if (transform.position.x <= -12f) 
+        { 
+            float randPosY = Random.Range(-3.75f, 6f);
+            transform.position = new Vector3(12f, randPosY, 0);
+        }
+
+    }
+}
