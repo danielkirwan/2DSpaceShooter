9a1c09890866effd9ae51b9c6852256a633645b8
Daniel Kirwan <danielkirwan07@gmail.com>
2021-03-29 22:06:01 +0100

    Adding scrolling background, needs a little more work

---
+++ 2D-SpaceShooter/Assets/Scripts/StarScroll.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StarScroll : MonoBehaviour
+{
+    private float _scrollSpeed = -5f;
+    private Vector2 _startPos;
+        
+    // Start is called before the first frame update
+    void Start()
+    {
+        _startPos = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float newPos = Mathf.Repeat(Time.time * _scrollSpeed, 20);
+        transform.position = _startPos + Vector2.right * newPos;
+    }
+}
