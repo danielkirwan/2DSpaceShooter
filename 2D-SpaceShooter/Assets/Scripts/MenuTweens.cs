bd512d04873edaf977266dda31c0abd44a721f8a
Daniel Kirwan <danielkirwan07@gmail.com>
2021-04-07 16:43:05 +0100

    Added buttons to menu

--- 2D-SpaceShooter/Assets/Scripts/MenuTweens.cs
+++ 2D-SpaceShooter/Assets/Scripts/MenuTweens.cs
@@ -1,22 +1,32 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MenuTweens : MonoBehaviour
 {
     [SerializeField] private GameObject _titleImage;
     [SerializeField] private GameObject _nameTitle;
+    [SerializeField] private GameObject _playButton;
+    [SerializeField] private GameObject _quitButton;
     // Start is called before the first frame update
     void Start()
     {
         MoveTitleText();
         MoveTitleImage();
+        MoveButtonPanel();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //1240.654   
+    }
+
+    public void MoveButtonPanel()
+    {
+        LeanTween.moveX(_playButton.GetComponent<RectTransform>(), 1522f, 1f).setEase(LeanTweenType.easeOutBack).setDelay(1f);
+        LeanTween.moveX(_quitButton.GetComponent<RectTransform>(), 1522f, 1f).setEase(LeanTweenType.easeOutBack).setDelay(2f);
     }
 
     public void MoveTitleText()
@@ -27,7 +37,15 @@ public class MenuTweens : MonoBehaviour
     public void MoveTitleImage()
     {
         LeanTween.moveX(_titleImage.GetComponent<RectTransform>(), 0f, 3f).setEase(LeanTweenType.easeOutBounce).setDelay(1f);
-
     }
 
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("Game", LoadSceneMode.Single);
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
 }
