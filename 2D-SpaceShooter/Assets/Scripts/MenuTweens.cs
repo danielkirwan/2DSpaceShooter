b04c4dfa38cee94cbede46cc279b4ac125792c76
Daniel Kirwan <danielkirwan07@gmail.com>
2021-04-07 16:03:48 +0100

    Added menu scene and main image

---
+++ 2D-SpaceShooter/Assets/Scripts/MenuTweens.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuTweens : MonoBehaviour
+{
+    [SerializeField] private GameObject _titleTextObject;
+    // Start is called before the first frame update
+    void Start()
+    {
+        MoveTitleText();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void MoveTitleText()
+    {
+        LeanTween.moveX(_titleTextObject.GetComponent<RectTransform>(), 0f, 2f).setEase(LeanTweenType.easeOutBounce).setDelay(1f);
+    }
+
+}
