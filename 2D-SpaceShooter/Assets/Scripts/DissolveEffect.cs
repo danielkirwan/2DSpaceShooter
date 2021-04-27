956b963ceefba52bfc225495810c280a04725a79
Daniel Kirwan <danielkirwan07@gmail.com>
2021-04-27 16:28:58 +0100

    Created dissolve shader for shields

---
+++ 2D-SpaceShooter/Assets/Scripts/DissolveEffect.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DissolveEffect : MonoBehaviour
+{
+    [SerializeField] private Material _material;
+    private float _dissolveAmount;
+    private bool _isDissolving;
+    private float _dissolveSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (_isDissolving)
+        {
+            _dissolveAmount = Mathf.Clamp01(_dissolveAmount + _dissolveSpeed * Time.deltaTime);
+            _material.SetFloat("_dissolveAmount", _dissolveAmount);
+        }
+        else
+        {
+            _dissolveAmount = Mathf.Clamp01(_dissolveAmount - _dissolveSpeed * Time.deltaTime);
+            _material.SetFloat("_dissolveAmount", _dissolveAmount);
+        }
+
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            _isDissolving = true;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Y))
+        {
+            _isDissolving = false;
+        }
+    }
+
+    public void StartDissolve(float speed)
+    {
+        _isDissolving = true;
+        this._dissolveSpeed = speed;
+    }
+
+    public void StopDissolve(float speed)
+    {
+        _isDissolving = false;
+        this._dissolveSpeed = speed;
+    }
+
+
+
+}
